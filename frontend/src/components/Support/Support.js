9703f885fad999944b89feab9fdf567cf33f7a77
Carlos Loureda <carlos.loureda@quobis.com>
2020-06-07 16:20:14 +0200

    feat: Added fetchMembers and fetchUsers for duplicated members/users

--- frontend/src/components/Support/Support.js
+++ frontend/src/components/Support/Support.js
@@ -19,7 +19,7 @@ const Support = () => {
               //   Authorization: "Bearer " + token,
             },
             body: JSON.stringify({
-              username: "user1",
+              username: "user5",
             }),
           }
         );
@@ -34,16 +34,15 @@ const Support = () => {
         //   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTE0Njc5MTAsImp0aSI6IjBlM2VhNjUwLWE4MjMtMTFlYS04ODdkLTNkNTg0NjVkNjcyZSIsImV4cCI6MTU5MTU1NDMxMCwiYWNsIjp7InBhdGhzIjp7Ii8qL3VzZXJzLyoqIjp7fSwiLyovY29udmVyc2F0aW9ucy8qKiI6e30sIi8qL3Nlc3Npb25zLyoqIjp7fSwiLyovZGV2aWNlcy8qKiI6e30sIi8qL2ltYWdlLyoqIjp7fSwiLyovbWVkaWEvKioiOnt9LCIvKi9hcHBsaWNhdGlvbnMvKioiOnt9LCIvKi9wdXNoLyoqIjp7fSwiLyova25vY2tpbmcvKioiOnt9fX0sImFwcGxpY2F0aW9uX2lkIjoiMzA2ZWUwZGItOTI0NC00ZGUyLWExNDktZWZjNTBlNWZjZWNkIiwic3ViIjoidXNlcjEifQ.AIV9FvG8pdzTeRG7wr3qSrH4rXmj-QyJd0ZvNc-MRAFa5QbY6JwkeX6yyF0LbqvBiUpfMfLgIlSGTZ1l_4pQTbKfdOd5sacDaIsD51EO_71wQERhXRXzhTbknLZY9LtxpRx136S-Z710WTay93Bj-_cYH7KTUMrNqT6_kJ1N3VbEkvD6EqgoJirbn1hmyNEWOBWz-HnIi-dEGXk2mwb0dNV7aojcZ7GGEh6glvS1ej7_kcdWv0jliUB9B9vcuOD096Gi9A5f5XbzmzyJafY3pNOcfrq_A7tiDWiv2UHjT5AN9JVUtN7pqPtBYcH3ptZIr6OVwK935D_9dpCy8leJGQ";
         let app = await client.login(token);
         // console.log("********************************************************");
-        // conversation = await app.getConversation(conversationId);
-        // console.log("========================================================");
-        // return conversation;
+        let conversation = await app.getConversation(conversationId);
+        console.log("--> conversation: ", conversation);
+        return conversation;
       } catch (error) {
         console.log("Request failed", error);
         return error;
       }
     }
     const conversation = getToken();
-    console.log("--> conversation: ", conversation);
   }, []);
   return <div>Hi from Support</div>;
 };
